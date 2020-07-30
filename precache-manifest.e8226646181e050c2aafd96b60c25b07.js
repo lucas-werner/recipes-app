4ab6712d1afa6c2cde8c37243197005d09e396c0
Lucas Werner <lucas.1werner@gmail.com>
2020-07-30 13:24:35 -0300

    Updates

---
+++ precache-manifest.e8226646181e050c2aafd96b60c25b07.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d0b83191d0351e0fc5af62e3ad954e",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "e1f71c0327a10f6c8d20",
+    "url": "/recipes-app/static/css/main.38984399.chunk.css"
+  },
+  {
+    "revision": "22d39a676f957fb0aeb6",
+    "url": "/recipes-app/static/js/2.e1146d14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipes-app/static/js/2.e1146d14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f71c0327a10f6c8d20",
+    "url": "/recipes-app/static/js/main.faa71ba3.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "a25b124f2942386837a44518b8563ada",
+    "url": "/recipes-app/static/media/cooking.a25b124f.gif"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
